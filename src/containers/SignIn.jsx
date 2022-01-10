9f1974aefe2e1050fd90833fe5012d9b778e1b04
John Kim A. Querobines <querobinesj@gmail.com>
2022-01-10 17:01:16 +0800

    fixes sign In and sign Up

--- src/containers/SignIn.jsx
+++ src/containers/SignIn.jsx
@@ -11,7 +11,7 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 import { useAuth } from "../contexts/AuthContext";
 
@@ -21,81 +21,83 @@ export default function SignIn() {
   const [isClicked, setIsClicked] = React.useState(false);
   const [errorMsg, setErrorMsg] = React.useState("");
   const [isVisible, setVisibility] = React.useState(false);
-  const { signIn } = useAuth();
+  const { signIn, user } = useAuth();
 
   const handleClick = async () => {
     const myMessage = await signIn(email, pass);
     setErrorMsg(myMessage);
-    console.log(errorMsg);
     setIsClicked(true);
   };
 
-  return (
-    <React.Fragment>
-      <Container>
-        <Grid container direction="column" alignItems="center" spacing={3}>
-          <Grid item container>
-            <Typography variant="h2" component="div" gutterBottom mt={5}>
-              Sign In
-            </Typography>
+  if (!user)
+    return (
+      <React.Fragment>
+        <Container>
+          <Grid container direction="column" alignItems="center" spacing={3}>
+            <Grid item container>
+              <Typography variant="h2" component="div" gutterBottom mt={5}>
+                Sign In
+              </Typography>
+            </Grid>
+            <Grid item container>
+              {errorMsg ? <Alert severity="error">{errorMsg}</Alert> : null}
+            </Grid>
+            <Grid item container>
+              <TextField
+                id="email"
+                label="Email"
+                helperText={email ? null : "Please enter your email"}
+                error={isClicked ? (email ? false : true) : null}
+                variant="filled"
+                fullWidth={true}
+                type="email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item container>
+              <TextField
+                id="password"
+                label="Password"
+                helperText={pass ? null : "Please enter your password"}
+                error={isClicked ? (pass ? false : true) : null}
+                variant="filled"
+                fullWidth={true}
+                type={isVisible ? "text" : "password"}
+                value={pass}
+                onChange={(event) => {
+                  setPass(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setVisibility(!isVisible)}
+                      >
+                        {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item container>
+              <Button variant="contained" onClick={() => handleClick()}>
+                LOGIN
+              </Button>
+            </Grid>
+            <Grid item container>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Dont have an account yet?{" "}
+                <Link to="/register">Sign up here</Link>
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item container>
-            {errorMsg ? <Alert severity="error">{errorMsg}</Alert> : null}
-          </Grid>
-          <Grid item container>
-            <TextField
-              id="email"
-              label="Email"
-              helperText={email ? null : "Please enter your email"}
-              error={isClicked ? (email ? false : true) : null}
-              variant="filled"
-              fullWidth={true}
-              type="email"
-              value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-            />
-          </Grid>
-          <Grid item container>
-            <TextField
-              id="password"
-              label="Password"
-              helperText={pass ? null : "Please enter your password"}
-              error={isClicked ? (pass ? false : true) : null}
-              variant="filled"
-              fullWidth={true}
-              type={isVisible ? "text" : "password"}
-              value={pass}
-              onChange={(event) => {
-                setPass(event.target.value);
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setVisibility(!isVisible)}
-                    >
-                      {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item container>
-            <Button variant="contained" onClick={() => handleClick()}>
-              LOGIN
-            </Button>
-          </Grid>
-          <Grid item container>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              Dont have an account yet? <Link to="/register">Sign up here</Link>
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    </React.Fragment>
-  );
+        </Container>
+      </React.Fragment>
+    );
+  else return <Navigate to="/" />;
 }
--- src/containers/SignUp.jsx
+++ src/containers/SignUp.jsx
@@ -11,7 +11,7 @@ import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 import { useAuth } from "../contexts/AuthContext";
 
@@ -22,110 +22,111 @@ export default function SimpleContainer() {
   const [isClicked, setIsClicked] = React.useState(false);
   const [errorMsg, setErrorMsg] = React.useState("");
   const [isVisible, setVisibility] = React.useState(false);
-  const { signUp } = useAuth();
+  const { signUp, user } = useAuth();
 
   const handleClick = async () => {
     const myMessage = await signUp(email, pass, pass2);
     setErrorMsg(myMessage);
-    console.log(errorMsg);
     setIsClicked(true);
   };
 
-  return (
-    <React.Fragment>
-      <Container>
-        <Grid container direction="column" alignItems="center" spacing={3}>
-          <Grid item container>
-            <Typography variant="h2" component="div" gutterBottom mt={5}>
-              Sign Up
-            </Typography>
+  if (!user)
+    return (
+      <React.Fragment>
+        <Container>
+          <Grid container direction="column" alignItems="center" spacing={3}>
             <Grid item container>
-              {errorMsg ? <Alert severity="error">{errorMsg}</Alert> : null}
+              <Typography variant="h2" component="div" gutterBottom mt={5}>
+                Sign Up
+              </Typography>
+              <Grid item container>
+                {errorMsg ? <Alert severity="error">{errorMsg}</Alert> : null}
+              </Grid>
+            </Grid>
+            <Grid item container>
+              <TextField
+                id="email"
+                label="Email"
+                helperText={email ? null : "Please enter your email"}
+                error={isClicked ? (email ? false : true) : null}
+                variant="filled"
+                fullWidth={true}
+                type="email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item container>
+              <TextField
+                id="password"
+                label="Password"
+                helperText={pass ? null : "Please enter your password"}
+                error={isClicked ? (pass ? false : true) : null}
+                variant="filled"
+                fullWidth={true}
+                type={isVisible ? "text" : "password"}
+                value={pass}
+                onChange={(event) => {
+                  setPass(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setVisibility(!isVisible)}
+                      >
+                        {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item container>
+              <TextField
+                id="password2"
+                label="Password Confirmation"
+                helperText={
+                  pass2 ? null : "Please enter your password confirmation"
+                }
+                error={isClicked ? (pass2 ? false : true) : null}
+                variant="filled"
+                fullWidth={true}
+                type={isVisible ? "text" : "password"}
+                value={pass2}
+                onChange={(event) => {
+                  setPass2(event.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setVisibility(!isVisible)}
+                      >
+                        {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item container>
+              <Button variant="contained" onClick={() => handleClick()}>
+                REGISTER
+              </Button>
+            </Grid>
+            <Grid item container>
+              <Typography variant="subtitle1" gutterBottom component="div">
+                Already have an account? <Link to="/login">Sign in here</Link>
+              </Typography>
             </Grid>
           </Grid>
-          <Grid item container>
-            <TextField
-              id="email"
-              label="Email"
-              helperText={email ? null : "Please enter your email"}
-              error={isClicked ? (email ? false : true) : null}
-              variant="filled"
-              fullWidth={true}
-              type="email"
-              value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-            />
-          </Grid>
-          <Grid item container>
-            <TextField
-              id="password"
-              label="Password"
-              helperText={pass ? null : "Please enter your password"}
-              error={isClicked ? (pass ? false : true) : null}
-              variant="filled"
-              fullWidth={true}
-              type={isVisible ? "text" : "password"}
-              value={pass}
-              onChange={(event) => {
-                setPass(event.target.value);
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setVisibility(!isVisible)}
-                    >
-                      {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item container>
-            <TextField
-              id="password2"
-              label="Password Confirmation"
-              helperText={
-                pass2 ? null : "Please enter your password confirmation"
-              }
-              error={isClicked ? (pass2 ? false : true) : null}
-              variant="filled"
-              fullWidth={true}
-              type={isVisible ? "text" : "password"}
-              value={pass2}
-              onChange={(event) => {
-                setPass2(event.target.value);
-              }}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={() => setVisibility(!isVisible)}
-                    >
-                      {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item container>
-            <Button variant="contained" onClick={() => handleClick()}>
-              LOGIN
-            </Button>
-          </Grid>
-          <Grid item container>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              Already have an account? <Link to="/login">Sign in here</Link>
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    </React.Fragment>
-  );
+        </Container>
+      </React.Fragment>
+    );
+  else return <Navigate to="/" />;
 }
