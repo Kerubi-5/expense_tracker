766ad44d63d3b318f4aa4fe978170cb6120badb8
John Kim A. Querobines <querobinesj@gmail.com>
2022-01-12 14:37:13 +0800

    removes Timestamp

--- src/components/ItemDialog.jsx
+++ src/components/ItemDialog.jsx
@@ -14,7 +14,7 @@ import Button from "@mui/material/Button";
 import Calendar from "./Calendar";
 
 import { useAuth } from "../contexts/AuthContext";
-import { doc, setDoc, addDoc, Timestamp } from "firebase/firestore";
+import { doc, setDoc, addDoc } from "firebase/firestore";
 import { expensesRef } from "../utils/firebase";
 
 import { useState } from "react";
