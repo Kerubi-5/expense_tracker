7f74ced13c86d8ed90e88ddcc6c13a9bcd9e0920
John Kim A. Querobines <querobinesj@gmail.com>
2022-01-12 21:17:41 +0800

    add fund functionality

--- src/containers/Wallet.jsx
+++ src/containers/Wallet.jsx
@@ -2,8 +2,42 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+import { useState, useEffect } from "react";
+
+import { useAuth } from "../contexts/AuthContext";
+
+import { walletRef } from "../utils/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const Wallet = () => {
+  const [wallet, setWallet] = useState(0);
+  const [funds, setFunds] = useState("");
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const getData = async () => {
+      const walletDoc = doc(walletRef, user.uid);
+      const walletData = await getDoc(walletDoc);
+      setWallet(walletData.data());
+      console.log("mice");
+    };
+    return getData();
+  }, []);
+
+  const addFunds = async () => {
+    const walletDoc = doc(walletRef, user.uid);
+
+    await setDoc(walletDoc, {
+      money: Number(wallet.money) + Number(funds),
+    });
+
+    setWallet({ money: Number(wallet.money) + Number(funds) });
+    setFunds("");
+  };
+
   return (
     <Container maxWidth="fluid">
       <Stack spacing={3}>
@@ -20,11 +54,22 @@ const Wallet = () => {
             Your remaining balance
           </Typography>
           <Typography variant="h5" component="div">
-            &#8369;10000
+            &#8369; {wallet.money ? wallet.money : ""}
           </Typography>
         </div>
 
-        <Button variant="contained">ADD MORE FUNDS</Button>
+        <TextField
+          id="addFunds"
+          label="Funds"
+          variant="standard"
+          type="number"
+          value={funds}
+          onChange={(event) => setFunds(event.target.value)}
+        />
+
+        <Button variant="contained" onClick={() => addFunds()}>
+          ADD MORE FUNDS
+        </Button>
       </Stack>
     </Container>
   );
